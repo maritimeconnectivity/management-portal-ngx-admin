6d04ab8d78c40f2ffce1ee6c83f100d6c4d2e4fd
Jinki Jung <your.jinki.jung@gmail.com>
2022-05-09 16:15:23 +0200

    keep the portal work for test env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,15 +3,15 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-export const environment = {
-  production: true,
+ export const environment = {
+  production: false,
   staging: false,
-  irBasePath: 'https://api.maritimeconnectivity.net',
-  srBasePath: '',
-  oidcBasePath: 'https://maritimeid.maritimeconnectivity.net',
-  hasServiceRegistry: false,
-  environmentTitle: 'MCC Testbed',
+  irBasePath: 'https://test-api.maritimeconnectivity.net',
+  oidcBasePath: 'https://test-maritimeid.maritimeconnectivity.net',
+  hasServiceRegistry: true,
+  srBasePath: 'https://msr-test.maritimeconnectivity.net',
+  environmentTitle: 'MCC Testbed TEST',
   termsOfUse: 'By applying for access to the MCP testbed, you agree not to store any personal information on the platform such as names and email addresses. Please use generic names, such as \'John Doe\' and generic email addresses such as \'info@company.com\'. It does need to be a working email address though, since access will be granted through this email address. Furthermore, if anyone chooses to federate an identity registry into the MCP testbed, this should only contain test data - not actual personal information. This is due to the European Union General Data Protection Regulation (GDPR). For more information, contact the MCC secretariat.',
   idpNamespace: 'mcc-test',
-  environmentName: 'public-demonstrator',
+  environmentName: 'test',
 };
