17278fd7475a0dc343a83c839038c88037e9c8cc
Jinki Jung <your.jinki.jung@gmail.com>
2022-05-10 10:37:28 +0200

    update setup and guide for test and public-demo deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,14 +4,14 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
  export const environment = {
-  production: false,
+  production: true,
   staging: false,
-  irBasePath: 'https://test-api.maritimeconnectivity.net',
-  oidcBasePath: 'https://test-maritimeid.maritimeconnectivity.net',
-  hasServiceRegistry: true,
-  srBasePath: 'https://msr-test.maritimeconnectivity.net',
-  environmentTitle: 'MCC Testbed TEST',
+  irBasePath: 'https://api.maritimeconnectivity.net',
+  srBasePath: '',
+  oidcBasePath: 'https://maritimeid.maritimeconnectivity.net',
+  hasServiceRegistry: false,
+  environmentTitle: 'MCC Testbed',
   termsOfUse: 'By applying for access to the MCP testbed, you agree not to store any personal information on the platform such as names and email addresses. Please use generic names, such as \'John Doe\' and generic email addresses such as \'info@company.com\'. It does need to be a working email address though, since access will be granted through this email address. Furthermore, if anyone chooses to federate an identity registry into the MCP testbed, this should only contain test data - not actual personal information. This is due to the European Union General Data Protection Regulation (GDPR). For more information, contact the MCC secretariat.',
-  idpNamespace: 'mcc-test',
-  environmentName: 'test',
+  idpNamespace: 'mcc',
+  environmentName: 'public-demonstrator',
 };
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -9,14 +9,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
-  staging: false,
-  irBasePath: 'https://api.maritimeconnectivity.net',
-  srBasePath: '',
-  oidcBasePath: 'https://maritimeid.maritimeconnectivity.net',
-  hasServiceRegistry: false,
-  environmentTitle: 'MCC Testbed',
+  production: false,
+  staging: true,
+  irBasePath: 'https://test-api.maritimeconnectivity.net',
+  oidcBasePath: 'https://test-maritimeid.maritimeconnectivity.net',
+  hasServiceRegistry: true,
+  srBasePath: 'https://msr-test.maritimeconnectivity.net',
+  environmentTitle: 'MCC Testbed STAGING',
   termsOfUse: 'By applying for access to the MCP testbed, you agree not to store any personal information on the platform such as names and email addresses. Please use generic names, such as \'John Doe\' and generic email addresses such as \'info@company.com\'. It does need to be a working email address though, since access will be granted through this email address. Furthermore, if anyone chooses to federate an identity registry into the MCP testbed, this should only contain test data - not actual personal information. This is due to the European Union General Data Protection Regulation (GDPR). For more information, contact the MCC secretariat.',
   idpNamespace: 'mcc-test',
-  environmentName: 'public-demonstrator',
+  environmentName: 'stage',
 };
