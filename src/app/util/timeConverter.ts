d335fdd7007b67ffe64f2b2f98d7fea632242398
Jinki Jung <your.jinki.jung@gmail.com>
2022-05-19 21:15:58 +0200

    prevent undefined error for time

--- src/app/util/timeConverter.ts
+++ src/app/util/timeConverter.ts
@@ -1,8 +1,8 @@
 export const convertTime = (s, timeZone = 'UTC'): string => {
+  if (s) {
     const dtFormat = new Intl.DateTimeFormat('en-GB', {
       timeZone: timeZone,
     });
-    
     return new Date(s).toISOString().slice(0, 19).replace('T', ' ');
-    // return formatDate(new Date(parseInt(s)),'MMM d, y hh:mm:ss', 'en_GB')
   }
+}
