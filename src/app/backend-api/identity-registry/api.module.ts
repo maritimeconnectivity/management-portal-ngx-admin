1c08657f9c028970bda29044a4b0ff1cf59a60d0
Jinki <your.jinki.jung@gmail.com>
2023-09-26 16:02:03 +0200

    setting ir base path

--- src/app/backend-api/identity-registry/api.module.ts
+++ src/app/backend-api/identity-registry/api.module.ts
@@ -14,23 +14,98 @@ import { ServiceControllerService } from './api/serviceController.service';
 import { UserControllerService } from './api/userController.service';
 import { VesselControllerService } from './api/vesselController.service';
 import { VesselImageControllerService } from './api/vesselImageController.service';
+import { AppConfig } from '../../app.config';
 
 @NgModule({
   imports:      [],
   declarations: [],
   exports:      [],
   providers: [
-    AgentControllerService,
-    CertificateControllerService,
-    DeviceControllerService,
-    LogoControllerService,
-    MmsControllerService,
-    OrganizationControllerService,
-    RoleControllerService,
-    ServiceControllerService,
-    UserControllerService,
-    VesselControllerService,
-    VesselImageControllerService ]
+    {
+        provide: AgentControllerService,
+        useFactory: (http: HttpClient) => {
+          return new AgentControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: AgentControllerService,
+        useFactory: (http: HttpClient) => {
+          return new AgentControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: CertificateControllerService,
+        useFactory: (http: HttpClient) => {
+          return new CertificateControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: DeviceControllerService,
+        useFactory: (http: HttpClient) => {
+          return new DeviceControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: LogoControllerService,
+        useFactory: (http: HttpClient) => {
+          return new LogoControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: MmsControllerService,
+        useFactory: (http: HttpClient) => {
+          return new MmsControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: OrganizationControllerService,
+        useFactory: (http: HttpClient) => {
+          return new OrganizationControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: RoleControllerService,
+        useFactory: (http: HttpClient) => {
+          return new RoleControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: ServiceControllerService,
+        useFactory: (http: HttpClient) => {
+          return new ServiceControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: UserControllerService,
+        useFactory: (http: HttpClient) => {
+          return new UserControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: VesselControllerService,
+        useFactory: (http: HttpClient) => {
+          return new VesselControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    {
+        provide: VesselImageControllerService,
+        useFactory: (http: HttpClient) => {
+          return new VesselImageControllerService(http, AppConfig.IR_BASE_PATH, null);
+      },
+        deps: [HttpClient]
+    },
+    ]
 })
 export class ApiModule {
     public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
